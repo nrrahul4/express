00701df7b27c83e6659e54f2a27d1c24520f374c
nrrahul4 <nrrahul4@gmail.com>
2020-08-06 22:45:50 +0530

    Added style to project

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const adminRoute  = require('./routes/admin');
 const shopRoute  = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoute);
 app.use(shopRoute);
