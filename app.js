9fbc3e1a5cd791df19ff4f329df3eef0ba6665e0
nrrahul4 <nrrahul4@gmail.com>
2020-08-07 13:35:34 +0530

    Pug implementation, shop converted to pug format

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const bodyParser = require('body-parser');
 const path  = require('path');
 const app = express();
 
-const adminRoute  = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData  = require('./routes/admin');
 const shopRoute  = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoute);
+app.use('/admin', adminData.router);
 app.use(shopRoute);
 
 app.use((req, res) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,20 @@
-const path  = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../utils/path');
+const rootDir = require("../utils/path");
 
 const router = express.Router();
+const products = [];
 
 router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
-  
+
 router.post("/product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
