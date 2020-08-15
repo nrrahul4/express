36b563200390ce8c802af244dca9bfad3d8949db
nrrahu4 <nrrahu4@gmail.com>
2020-08-16 01:38:35 +0530

    Edit and delete for products.

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing: false,
   });
 };
 
@@ -15,17 +13,49 @@ exports.postAddProduct = (req, res) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, +price);
   product.save();
   res.redirect('/');
 };
 
+exports.getEditProduct = (req, res) => {
+  const { edit = 'false' } = req.query;
+  if (edit === 'false') {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      product,
+      editing: edit,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res) => {
+  const { productId, title, imageUrl, price, description } = req.body; 
+  const product = new Product(productId, title, imageUrl, description, price);
+  product.save();
+  res.redirect('/admin/products');
+};
+
+exports.postDeleteProduct = (req, res) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+};
+
 exports.getProducts = (req, res) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
-      path: '/admin/products'
+      path: '/admin/products',
     });
   });
 };
